998fe321f20dc1476f642027f86afa78dda6fbd6
Miqesta <116167904+Miqesta@users.noreply.github.com>
2025-02-11 22:40:30 +0400

    Add files via upload
    some changes with responsive

--- tscript.js
+++ tscript.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Select all elements to animate
-    var slideElements = document.querySelectorAll('.tcontainer, .t-container, .ts-rightside');
+    var slideElements = document.querySelectorAll('.ts-rightside, .trightpayment, .tpay-box, .tcontainer, .t-container');
 
     // Intersection Observer setup
-    var observer = new IntersectionObserver(function(entries, observer) {
+    var observer = new IntersectionObserver(function(entries) {
         entries.forEach(function(entry) {
             if (entry.isIntersecting) {
                 entry.target.classList.add('slideIn');
-                observer.unobserve(entry.target); // Stop observing once animation starts
+            } else {
+                entry.target.classList.remove('slideIn'); // Allows re-triggering
             }
         });
     }, { threshold: 0.2 }); // Trigger when 20% of element is visible
